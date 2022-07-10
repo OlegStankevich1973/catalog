c694a908efaf2faa95c553d9279876ae4b6614d3
Oleg Stankevich <stankeviz@mail.ru>
2022-07-10 18:14:22 +0300

    Add the program for displaying the name on the screen

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine(" Hello, World!");
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет!"); 
+Console.WriteLine(username);
+{
+    
+}
