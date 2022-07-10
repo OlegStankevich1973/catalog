a19fe64e01525ab27760bc849702f6c58ec0ae6a
Oleg Stankevich <stankeviz@mail.ru>
2022-07-10 23:12:50 +0300

    Программа избранного приветствия

---
+++ Ex008/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите своё имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}    
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
